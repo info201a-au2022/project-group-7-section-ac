fdbb2daff2e579857693fcbe8b8d1d834e6c46a6
Darin Nguyen <darinnguyen@Darins-MacBook-Pro.local>
2022-11-15 23:48:05 -0800

    Final final Changes

--- source/table.R
+++ source/table.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 
-disaster_details <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-7-section-ac/main/data/Billion-Dollar%20Weather%20and%20Climate%20Disasters%20Events%20Aug%202021.csv")
+disaster_details <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-7-section-ac/main/data/Billion_Dollar_Weather_and_Climate_Disasters_Events_Aug_2021.csv")
 usa_natural_disasters <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-7-section-ac/main/data/Natural_Disaster_declaration.csv")
 
 usa_table <- usa_natural_disasters %>% 
