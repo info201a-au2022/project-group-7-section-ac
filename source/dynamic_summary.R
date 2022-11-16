8ed1ab1f345e59d104eff3ad19ca21c83b044e44
Roh Sarma <rohsarma@Rohs-MacBook-Air.local>
2022-11-15 23:39:35 -0800

    progression

--- source/dynamic_summary.R
+++ source/dynamic_summary.R
@@ -3,8 +3,7 @@ library(dplyr)
 library(ggplot2)
 
 usa_natural_disasters <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-7-section-ac/main/data/Natural_Disaster_declaration.csv")
-disaster_details <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-7-section-ac/main/data/Billion-Dollar%20Weather%20and%20Climate%20Disasters%20Events%20Aug%202021.csv")
-
+disaster_details <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-7-section-ac/main/data/Billion_Dollar_Weather_and_Climate_Disasters_Events_Aug_2021.csv")
 
 get_max_deaths <- function(){
   max_deaths <- disaster_details %>%
@@ -46,4 +45,19 @@ bp
 pie <- bp + coord_polar("y", start = 0)
 pie
 
+top7 <- data.frame(
+  States = c("Texas", "Missouri", "Kentucky", "Virginia", "Oklahoma", "Iowa", "Georgia"),
+  Disasters = c(3842, 2263, 2026, 1982, 1882, 1471, 1406 )
+)
+head(top7)
+
+p<-ggplot(data=top7, aes(x = States, y = Disasters)) +
+  geom_bar(stat="identity", fill="steelblue")+
+  theme_minimal() +
+  labs(x = "States", y = "Amount of Disasters", title = "Top 7 Most Disastrous States")
+p
+
+usa_table <- usa_natural_disasters %>% 
+  group_by(State) %>% 
+  summarize(max_date = max(Declaration.Date))
 
